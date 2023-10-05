d7551a2e543da5b13aff8933b2c20d200252c765
Gokul-96 <gokulakrishnan.m96@gmail.com>
2023-10-05 20:27:21 +0530

    formik

---
+++ src/component/Footer.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+
+            <div className="container-fluid  m-2 rounded">
+                <p className='fw-bold text-center'>Copyright @ Boobathi 2023</p>
+
+            </div>
+
+
+
+        </>
+    )
+}
+
+export default Footer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
